0a670f32e50c573a34efec5f4d0cb9d31b31f6ce
Michael Black <mwboct16@gmail.com>
2019-03-07 21:09:23 -0500

    File path tree and pseudocode for project

---
+++ assets/javascript/script.js
@@ -0,0 +1,27 @@
+/*
+NOT IN ANY PARTICULAR ORDER 
+=============================
+
+- Create function that grabs the results from the API(s)
+    - Create function that randomizes the results in the API containing restuarant info.
+    - Make an on-click event, that displays the random result when the user hits the "Try Again" button
+
+- Create a function that grabs the results of the zip-code and radius from the geolocation API
+    - Figure out a way to get restaurants within a certain radius and display them using the food API
+
+- Use firebase to store user's information and their inputs, store recommended restaurants.
+
+- When the user clicks a certain drop-down, to only display those as part of the randomizer
+    - Example: When user hits 'Fast Food' => Only display fast-food restaurants
+    - Check for keywords like 'type of cuisine/food in the API Documentation'
+
+--------
+Bonus
+----------
+If we get far enough:
+- If user wants to display the menu, to display a link to the menu or a picture. Create options for those also.
+
+/*
+
+
+
